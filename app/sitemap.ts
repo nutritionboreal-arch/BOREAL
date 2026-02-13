001932f3a2b1dbfd5969fbb2c751e22ed3211719
Boreal <nutrition.boreal@gmail.com>
2026-02-13 15:23:13 -0500

    update sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -12,6 +12,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 1,
     },
     {
+      url: `${siteUrl}/whey-isolate-canada`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+    {
       url: `${siteUrl}/privacy`,
       lastModified: new Date(),
       changeFrequency: "yearly",
